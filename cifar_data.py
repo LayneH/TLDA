576bebb01723dcbffe3c93647dd23dca10d64fcb
Layne H <Layne H>
2017-05-14 15:29:20 +0800

    add parser

--- cifar_data.py
+++ cifar_data.py
@@ -22,22 +22,23 @@ def load_CIFAR_batch(filename):
 def load_CIFAR100(batch_dir):
     """ load all of cifar """
     ims, coarse_labels, fine_labels = load_CIFAR_batch(batch_dir + '/train')
-    ims_t, c_labels, f_labels = load_CIFAR_batch(batch_dir + '/test')   
+    ims_t, c_labels, f_labels = load_CIFAR_batch(batch_dir + '/test')
     ims = np.concatenate((ims, ims_t))
     coarse_labels = np.concatenate((coarse_labels, c_labels))
     fine_labels = np.concatenate((fine_labels, f_labels))
     return ims, coarse_labels, fine_labels
 
-def load_data(fname = 'datasets/cifar100-reordered.npy'):
+def load_data(types, cifar_dir = 'datasets'):
+    # reorder the cifar-100 dataset by its superclass
+    # see https://www.cs.toronto.edu/~kriz/cifar.html for details of classes
+    fname = cifar_dir.split('/')[0] + '/cifar100-reordered.npy'
     if os.path.isfile(fname) == False:
-        ims, coarse_labels, fine_labels = load_CIFAR100('datasets/cifar-100-python')
-        data = org_by_super_class(ims, coarse_labels, fine_labels, 'datasets/cifar-100-python/meta')
+        ims, coarse_labels, fine_labels = load_CIFAR100(cifar_dir)
+        data = org_by_super_class(ims, coarse_labels, fine_labels, cifar_dir + '/meta')
         with open(fname, 'wb') as fo:
             np.save(fo, data)
-    
-    types = ["fruit_and_vegetables", "household_electrical_devices"]
+
     data = {}
-    
     with open(fname, 'rb') as infile:
         d_temp = np.load(infile).item()
     for name in types:
@@ -46,7 +47,7 @@ def load_data(fname = 'datasets/cifar100-reordered.npy'):
         for kk in data[k]:
             # no reshape
             # data[k][kk] = np.transpose(data[k][kk].reshape((-1, 3, 32, 32)), (0, 2, 3, 1))
-            data[k][kk] = data[k][kk].astype(np.float32)
+            data[k][kk] = data[k][kk].astype(np.float32) / 255.
     return data
 
 def org_by_super_class(ims, coarse_labels, fine_labels, meta_dir):
@@ -55,9 +56,9 @@ def org_by_super_class(ims, coarse_labels, fine_labels, meta_dir):
         name_dict = cPickle.load(fo)
     coarse_names = name_dict['coarse_label_names']
     fine_names = name_dict['fine_label_names']
-    
+
     data_dict = {}
-    
+
     for i, name in enumerate(coarse_names):
         data_dict[name] = {}
         temp_labels = fine_labels[coarse_labels == i]
@@ -68,10 +69,10 @@ def org_by_super_class(ims, coarse_labels, fine_labels, meta_dir):
                 f_lbs.append(f_lb)
             if len(f_lbs) == 5:
                 break
-        
+
         for f_lb in f_lbs:
             data_dict[name][fine_names[f_lb]] = ims[fine_labels == f_lb]
-        
+
     return data_dict
 
 class data_holder(object):
@@ -81,7 +82,7 @@ class data_holder(object):
         self._index_in_epoch = 0
         self._images = images
         self._labels = labels
-    
+
     def next_batch(self, batch_size=100):
         start = self._index_in_epoch
         self._index_in_epoch += batch_size
@@ -95,16 +96,16 @@ class data_holder(object):
             assert batch_size <= self._images.shape[0], "Batch size must less equal to number of examples."
         end = self._index_in_epoch
         return self._images[start:end], self._labels[start:end]
-        
-    
+
+
     @property
     def images(self):
         return self._images
-    
+
     @property
     def labels(self):
         return self._labels
-    
+
     @property
     def size(self):
         return self._num_examples
